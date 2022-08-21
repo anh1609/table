4f3df6e2acae74f54ffe04790ac4e6e056ed526d
anh1609 <laxtienanh62@gmail.com>
2022-08-21 12:27:32 +0700

    update

--- project_tts/assets/main.js
+++ project_tts/assets/main.js
@@ -10,16 +10,36 @@ function myFunction() {
 }
 
 // thay doi anh
-var index = 1;
+// var index = 1;
 
-let change = function changeImg() {
+// let change = function changeImg() {
 
-    var imgs = ["./assets/img/changeimg/img1.jpg", "./assets/img/changeimg/img2.jpg", "./assets/img/changeimg/img3.jpg"]
-    document.getElementById("myimg").src = imgs[index];
-    index++;
-    if (index == 3) {
-        index = 0;
+//     var imgs = ["./assets/img/changeimg/img1.jpg", "./assets/img/changeimg/img2.jpg", "./assets/img/changeimg/img3.jpg"]
+//     document.getElementById("myimg").src = imgs[index];
+//     index++;
+//     if (index == 3) {
+//         index = 0;
+//     }
+
+
+// }
+// setInterval(change, 2000)
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
-setInterval(change, 2000)
